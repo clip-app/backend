8ecc5726b3c007aa3c7aa7cc1b5e5ef49f26c06e
Avi Romanoff <avi@romanoff.me>
2014-08-02 14:35:15 -0700

    naive word segmentation

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ var _       = require('underscore');
 var request = require('superagent');
 var xml2js  = require('xml2js');
 var Youtube = require('youtube-api');
+var ent     = require('ent');
+var async   = require('async');
 
-var client = redis.createClient();
+// var client = redis.createClient();
 
 Youtube.authenticate({
     type: 'key',
@@ -21,21 +23,38 @@ function getCaptionsFromVideoId(videoId) {
     if (err) {
       console.log(err);
     }
-    // console.log(res);
     xml2js.parseString(res.text, function (err, result) {
       // console.dir(result.transcript);
       var segments = _.map(result.transcript.text, function(textElement) {
         return {
-          text: textElement._,
+          text: ent.decode(textElement._).replace('\n', ' '),
           start: Number(textElement.$.start),
           end: Number(textElement.$.start) + Number(textElement.$.dur),
           dur: Number(textElement.$.dur)
         }
       });
       console.log(segments)
+      var wordMaps = _.flatten(_.map(segments, splitSegmentIntoWordMap));
+      console.log(wordMaps)
     });
 
   });
+}
+
+function splitSegmentIntoWordMap(segment) {
+  // var text = str.replace(
+  var words = segment.text.split(' ');
+  var avgWordDur = segment.dur / words.length;
+  return _.map(words, function(word) {
+    var start = segment.start + ((words.indexOf(word)) * avgWordDur);
+    var end = start + avgWordDur;
+    return {
+      word: word,
+      start: start,
+      end: end
+    }
+  }) 
+  console.log(avgWordDur);
 }
 
 getCaptionsFromVideoId('LCZ-cxfxzvk');
