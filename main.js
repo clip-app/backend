47904b64241dc818cee690e192774921a3e685a6
Avi Romanoff <avi@romanoff.me>
2014-08-02 19:41:13 -0700

    commit new version

--- main.js
+++ main.js
@@ -26,16 +26,34 @@ function getCaptionsFromVideoId(videoId) {
     xml2js.parseString(res.text, function (err, result) {
       // console.dir(result.transcript);
       var segments = _.map(result.transcript.text, function(textElement) {
+        var text = ent.decode(textElement._).replace('\n', ' ');
+        if (text.indexOf(':') !== -1) {
+          console.log('THOUGHT SPEAKER!!! (V)');
+          console.log(textElement.$);
+          text = text.split(':')[1].trim();
+          console.log([text]);
+        }
+        // function remove
+        if (text.indexOf('(') !== -1 || text.indexOf(')') !== -1 ||
+           text.indexOf('[') !== -1 || text.indexOf(']') !== -1) {
+          console.log("CRAP DETECTED!!! Skipping... (V)");
+          console.log([text]);
+          return null;
+
+      }
         return {
-          text: ent.decode(textElement._).replace('\n', ' '),
+          text: text,
           start: Number(textElement.$.start),
           end: Number(textElement.$.start) + Number(textElement.$.dur),
           dur: Number(textElement.$.dur)
         }
       });
-      console.log(segments)
+      segments = _.without(segments, null); // Remove skipped segments
+      // console.log(segments)
       var wordMaps = _.flatten(_.map(segments, splitSegmentIntoWordMap));
-      console.log(wordMaps)
+      // var wordMaps = _.map(segments, splitSegmentIntoWordMap);
+      // console.log(JSON.stringify(wordMaps))
+      console.log(JSON.stringify(wordMaps, undefined, 2))
     });
 
   });
@@ -45,16 +63,37 @@ function splitSegmentIntoWordMap(segment) {
   // var text = str.replace(
   var words = segment.text.split(' ');
   var avgWordDur = segment.dur / words.length;
+  var avgCharDur = segment.dur / segment.text.length;
+  // words with less characters take less time to say
+  // [i used to massively open small microscopes ]
+  // 0.00 - 5.60
+  // 0.80 per word
+  // 7 words
+  // 36 characters
+  // 0.156 per character
+  // used = 4 * 0.156 = 0.624
+  // massively = 11*0.156
+
   return _.map(words, function(word) {
-    var start = segment.start + ((words.indexOf(word)) * avgWordDur);
-    var end = start + avgWordDur;
+    var start_word = segment.start + ((words.indexOf(word)) * avgWordDur);
+    // var start_char = segment.start + (word.length * avgWordDur);
+    var start_char = segment.start + (segment.text.indexOf(word) * avgCharDur);
+    var end_word = start_char + avgWordDur;
+    // var end_char = start_char + avgWordDur;
     return {
       word: word,
-      start: Number(start.toFixed(3)),
-      end: Number(end.toFixed(3))
+      video_id: process.argv[2],
+      // start_word: Number(start_word.toFixed(3)),
+      end: Number(end_word.toFixed(3)),
+      // start_char: Number(start_char.toFixed(3)),
+      start: Number(start_char.toFixed(3)),
+      // end_char: end_char
     }
   }) 
   console.log(avgWordDur);
 }
 
-getCaptionsFromVideoId('2HFLwotYfl0');
+assert(process.argv[2], 'expected argv[2] (YouTube Video ID')
+// console.log(process.argv)
+
+getCaptionsFromVideoId(process.argv[2]);
